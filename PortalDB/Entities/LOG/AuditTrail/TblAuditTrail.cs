55eeaa8cc039f85496bbb430554255e4edc382f8
rysmjsh <reyesmjosh@gmail.com>
2025-10-16 10:24:31 +0800

    Updated gitignore

---
+++ PortalDB/Entities/LOG/AuditTrail/TblAuditTrail.cs
@@ -0,0 +1,44 @@
+﻿using PortalDB.Entities.DBO.Account;
+using PortalTools.Utilities;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PortalDB.Entities.LOG.AuditTrail
+{
+    [Table("tblAuditTrails", Schema = "log")]
+    public class TblAuditTrail
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("AuditTrailId")]
+        public int Id { get; set; }
+
+        [Column("AuditTrailTableName")]
+        public string? TableName { get; set; }
+
+        [Column("AuditTrailRecordId")]
+        public long? RecordId { get; set; }
+
+        [MaxLength(10)]
+        [Column("AuditTrailAction")]
+        public string? Action { get; set; }
+
+        [Column("AuditTrailChanges")]
+        public byte[]? ChangesEncrypted { get; set; }
+        [NotMapped]
+        public string? Changes
+        {
+            get => ChangesEncrypted == null || ChangesEncrypted.Length == 0 ? null : EncryptionHelper.DecryptFromBytes(ChangesEncrypted);
+            set => ChangesEncrypted = string.IsNullOrEmpty(value) ? null : EncryptionHelper.EncryptToBytes(value);
+        }
+
+        [Column("AuditTrailChangedBySystemUserId")]
+        public long? ChangedBy { get; set; }
+        //Temporarily removed due to issue when inserting new data/handled by the system
+        //[ForeignKey(nameof(ChangedBy))]
+        //public virtual TblSystemUser SystemUser { get; set; } = null!;
+
+        [Column("AuditTrailChangedAt")]
+        public DateTime? ChangedAt { get; set; } = DateTime.UtcNow;
+    }
+}
---
+++ PortalDB/Entities/LOG/TblErrorLog.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PortalDB.Entities.LOG
+{
+    [Table("tblErrorLogs", Schema = "log")]
+    public class TblErrorLog
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        [Column("ErrorLogId")]
+        public int Id { get; set; }
+
+        [MaxLength(30)]
+        [Column("ErrorLogFile")]
+        public string? File { get; set; }
+
+        [MaxLength(30)]
+        [Column("ErrorLogLine")]
+        public string? Line { get; set; }
+
+        [Column("ErrorLogDescription")]
+        public string? Description { get; set; }
+
+        [Column("ErrorLogCreatedAt")]
+        public DateTime? CreatedAt { get; set; } = DateTime.UtcNow;
+
+    }
+}
