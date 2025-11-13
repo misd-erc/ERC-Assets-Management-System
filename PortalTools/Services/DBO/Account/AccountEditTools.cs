26d464c9b4e12912f80bcb334d68903a08ecad1e
rysmjsh <reyesmjosh@gmail.com>
2025-11-13 10:55:56 +0800

    Updated adding of roles on validation

--- PortalTools/Services/DBO/Account/AccountEditTools.cs
+++ PortalTools/Services/DBO/Account/AccountEditTools.cs
@@ -42,7 +42,8 @@ namespace PortalTools.Services.DBO.Account
 
                 if (isInsert)
                 {
-                    model.SystemRoleId = _accountGetTools.GetSystemRoleByNameAsync(TblSystemRole.EMPLOYEE, context)?.Id;
+                    var role = await _accountGetTools.GetSystemRoleByNameAsync(TblSystemRole.EMPLOYEE, context);
+                    model.SystemRoleId = role?.Id;
                     model.StatusId = TblSystemUserStatus.Dictionary[TblSystemUserStatus.PENDING];
                     await context.TblSystemUsers.AddAsync(model);
                     await context.SaveChangesAsync();
