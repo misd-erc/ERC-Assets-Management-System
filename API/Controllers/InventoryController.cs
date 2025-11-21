d51658aec3fa83f51bbb0b1357c05581e434bb7e
rysmjsh <reyesmjosh@gmail.com>
2025-11-21 17:02:25 +0800

    Added Edit for PPE, Movement, and Part.

--- API/Controllers/InventoryController.cs
+++ API/Controllers/InventoryController.cs
@@ -7,8 +7,11 @@ using PortalDB.Entities.ASSET.PPE;
 using PortalDB.Entities.DBO.Account;
 using PortalDB.Entities.DBO.Office;
 using PortalDB.Models.ParserModels.PPE;
+using PortalDB.Models.QueryParams.Office;
 using PortalDB.Models.QueryParams.Pagination;
+using PortalDB.Models.QueryParams.PPE;
 using PortalDB.Models.QueryParams.Universal;
+using PortalDB.Models.ResponseModels.Account;
 using PortalDB.Models.ResponseModels.Office;
 using PortalDB.Models.ResponseModels.PPE;
 using PortalDB.Models.Responses;
@@ -475,6 +478,144 @@ namespace API.Controllers
                 await ErrorTool.ErrorLogAsync(new PortalDbContext(_options), ex, nameof(OfficeController));
                 return StatusCode(ApiStatusCode.BadRequest, ApiResponse<object>.Fail(ErrorCodes.SERVER_ERROR, "An error occurred while processing your request. Please check the template format."));
 
+            }
+        }
+
+        // POST api/inventory/ppe/edit
+        [HttpPost("ppe/edit")]
+        [ValidateSessionToken]
+        [ValidateModelRequiredFields]
+        public async Task<IActionResult> EditPPE([FromBody] EditPPEQueryParams model)
+        {
+            await using var context = new PortalDbContext(_options);
+            await using var transaction = await context.Database.BeginTransactionAsync();
+
+            try
+            {
+
+                TblPPE ppe = new()
+                {
+                    Id = model.Id,
+                    PropertyNumber = model.PropertyNumber,
+                    CategoryId = model.CategoryId,
+                    LegendId = model.LegendId,
+                    Description = model.Description,
+                    Brand = model.Brand,
+                    Model = model.Model,
+                    SerialNumber = model.SerialNumber,
+                    UnitOfMeasurement = model.UnitOfMeasurement,
+                    UnitValue = model.UnitValue,
+                    DateAcquired = model.DateAcquired,
+                    EstimatedUsefulLife = model.EstimatedUsefulLife,
+                    IsActive = model.IsActive
+                };
+
+                long ppeId = await _editTools.PPE.EditTblPPEAsync(ppe, model.ActionBySystemUserId, context);
+
+                UserSimplePublicResponseModel publicRM = new()
+                {
+                    SystemUserId = model.ActionBySystemUserId,
+                    SessionKey = model.SessionKey
+                };
+
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return Ok(ApiResponse<object>.Ok(publicRM, $"PPE has been {(model.Id == 0 ? "added" : "updated")}"));
+
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                await ErrorTool.ErrorLogAsync(new PortalDbContext(_options), ex, nameof(InventoryController));
+                return StatusCode(ApiStatusCode.InternalServerError, ApiResponse<object>.Fail(ErrorCodes.SERVER_ERROR, "An error occurred while processing your request."));
+            }
+        }
+
+        // POST api/inventory/ppe/part/edit
+        [HttpPost("ppe/part/edit")]
+        [ValidateSessionToken]
+        [ValidateModelRequiredFields]
+        public async Task<IActionResult> EditPPEPart([FromBody] EditPPEPartQueryParams model)
+        {
+            await using var context = new PortalDbContext(_options);
+            await using var transaction = await context.Database.BeginTransactionAsync();
+
+            try
+            {
+
+                TblPPEPart ppePart = new()
+                {
+                    Id = model.Id,
+                    Name = model.Name,
+                    SerialNumber = model.SerialNumber,
+                    IsActive = model.IsActive
+                };
+
+                long ppePartId = await _editTools.PPE.EditTblPPEPartAsync(ppePart, model.ActionBySystemUserId, context);
+
+                UserSimplePublicResponseModel publicRM = new()
+                {
+                    SystemUserId = model.ActionBySystemUserId,
+                    SessionKey = model.SessionKey
+                };
+
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return Ok(ApiResponse<object>.Ok(publicRM, $"PPE Part has been {(model.Id == 0 ? "added" : "updated")}"));
+
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                await ErrorTool.ErrorLogAsync(new PortalDbContext(_options), ex, nameof(InventoryController));
+                return StatusCode(ApiStatusCode.InternalServerError, ApiResponse<object>.Fail(ErrorCodes.SERVER_ERROR, "An error occurred while processing your request."));
+            }
+        }
+
+        // POST api/inventory/ppe/movement/edit
+        [HttpPost("ppe/movement/edit")]
+        [ValidateSessionToken]
+        [ValidateModelRequiredFields]
+        public async Task<IActionResult> EditPPEMovement([FromBody] EditPPEMovementQueryParams model)
+        {
+            await using var context = new PortalDbContext(_options);
+            await using var transaction = await context.Database.BeginTransactionAsync();
+
+            try
+            {
+
+                TblPPEMovement ppeMovement = new()
+                {
+                    Id = model.Id,
+                    PPEId = model.PPEId,
+                    DateAssigned = model.DateAssigned,
+                    PARITRNumber = model.ParItrNumber,
+                    PlantillaEmployeeId = model.PlantillaEmployeeId,
+                    NonPlantillaEmployeeId = model.NonPlantillaEmployeeId,
+                    ActualOfficeId = model.ActualOfficeId,
+                    ActualDivisionId = model.ActualDivisionId,
+                    Remarks = model.Condition,
+                    IsActive = model.IsActive
+                };
+
+                long ppeMovementId = await _editTools.PPE.EditTblPPEMovementAsync(ppeMovement, model.ActionBySystemUserId, context);
+
+                UserSimplePublicResponseModel publicRM = new()
+                {
+                    SystemUserId = model.ActionBySystemUserId,
+                    SessionKey = model.SessionKey
+                };
+
+                await context.SaveChangesAsync();
+                await transaction.CommitAsync();
+                return Ok(ApiResponse<object>.Ok(publicRM, $"PPE Movement has been {(model.Id == 0 ? "added" : "updated")}"));
+
+            }
+            catch (Exception ex)
+            {
+                await transaction.RollbackAsync();
+                await ErrorTool.ErrorLogAsync(new PortalDbContext(_options), ex, nameof(InventoryController));
+                return StatusCode(ApiStatusCode.InternalServerError, ApiResponse<object>.Fail(ErrorCodes.SERVER_ERROR, "An error occurred while processing your request."));
             }
         }
         #endregion
---
+++ PortalDB/Models/QueryParams/PPE/EditPPEMovementQueryParams.cs
@@ -0,0 +1,27 @@
+﻿using PortalDB.Entities.ASSET.PPE;
+using PortalDB.Entities.DBO.Office;
+using PortalDB.Entities.DBO.Office.Division;
+using PortalDB.Models.ResponseModels.PPE;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace PortalDB.Models.QueryParams.PPE
+{
+    public class EditPPEMovementQueryParams
+    {
+        [Required] public long Id { get; set; }
+        [Required] public long PPEId { get; set; }
+        [Required] public DateTime DateAssigned { get; set; }
+        [Required] public string ParItrNumber { get; set; } = string.Empty;
+        [Required] public long PlantillaEmployeeId { get; set; }
+        [Required] public long NonPlantillaEmployeeId { get; set; }
+        [Required] public string Condition { get; set; } = string.Empty;
+        [Required] public long ActualOfficeId { get; set; }
+        [Required] public long ActualDivisionId { get; set; }
+        [Required] public bool IsActive { get; set; }
+        [Required] public long ActionBySystemUserId { get; set; }
+        [Required] public string SessionKey { get; set; } = string.Empty;
+    }
+}
