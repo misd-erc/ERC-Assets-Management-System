4d2370939c77db2c9566ef1395e7c05c2f638716
rysmjsh <reyesmjosh@gmail.com>
2025-12-01 16:00:45 +0800

    New migrations TblDivision

---
+++ PortalDB/Migrations/20251201075938_UpdatedTblDivisionAcronym.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdatedTblDivisionAcronym : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DivisionAcronym",
+                schema: "dbo",
+                table: "tblDivisions",
+                type: "nvarchar(30)",
+                maxLength: 30,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(15)",
+                oldMaxLength: 15,
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "DivisionAcronym",
+                schema: "dbo",
+                table: "tblDivisions",
+                type: "nvarchar(15)",
+                maxLength: 15,
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(30)",
+                oldMaxLength: 30,
+                oldNullable: true);
+        }
+    }
+}
