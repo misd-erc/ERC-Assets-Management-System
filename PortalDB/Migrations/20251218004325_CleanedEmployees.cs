ddddf1eaffb318ea65bd72251b72d4c5d7106c61
rysmjsh <reyesmjosh@gmail.com>
2025-12-18 09:08:14 +0800

    Updated Employees

---
+++ PortalDB/Migrations/20251218004325_CleanedEmployees.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class CleanedEmployees : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            // Execute in a transaction for atomicity
+            migrationBuilder.Sql(@"
+                BEGIN TRANSACTION;
+
+                TRUNCATE TABLE asset.tblPTAParts;
+                TRUNCATE TABLE asset.tblPTAs;
+                TRUNCATE TABLE dbo.tblEmployees;
+
+                COMMIT TRANSACTION;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
---
+++ PortalDB/Migrations/20251218004908_CLeanedPTAMovements.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class CLeanedPTAMovements : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql(@"
+                BEGIN TRANSACTION;
+
+                TRUNCATE TABLE asset.tblPTAMovements;
+
+                COMMIT TRANSACTION;
+            ");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
