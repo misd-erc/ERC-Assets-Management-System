5b81fc12626fb6784e21977348b3687b85c4a7cf
rysmjsh <reyesmjosh@gmail.com>
2026-01-05 14:09:18 +0800

    Added dashboard api for pta

--- PortalCommon/Constants/AzureConstants.cs
+++ PortalCommon/Constants/AzureConstants.cs
@@ -14,8 +14,8 @@ namespace PortalCommon.Constants
         public static readonly string OBJECT_ID = EnvironmentConstants.ERC_AMS_AZURE_OBJECT_ID;
         public static readonly string CLIENT_SECRET_ID = EnvironmentConstants.ERC_AMS_AZURE_CLIENT_SECRET_ID;
         public static readonly string CLIENT_SECRET_VALUE = EnvironmentConstants.ERC_AMS_AZURE_CLIENT_SECRET_VALUE;
-        public static readonly string AUTHORITY = EnvironmentConstants.ERC_AMS_AZURE_AUTHORITY;
-        public static readonly string GRAPH_SCOPE = EnvironmentConstants.ERC_AMS_AZURE_GRAPH_SCOPE;
+        public static readonly string AUTHORITY = EnvironmentConstants.ERC_AMS_AZURE_AUTHORITY; //https://login.microsoftonline.com/{tenant}
+        public static readonly string GRAPH_SCOPE = EnvironmentConstants.ERC_AMS_AZURE_GRAPH_SCOPE; //https://graph.microsoft.com/.default
         public static readonly string BLOB_STORAGE_ACCOUNT_KEY = EnvironmentConstants.ERC_AMS_AZURE_BLOB_STORAGE_ACCOUNT_KEY;
         public static readonly string BLOB_STORAGE_ACCOUNT_NAME = EnvironmentConstants.ERC_AMS_AZURE_BLOB_STORAGE_ACCOUNT_NAME;
         public static readonly string BLOB_STORAGE_CONTAINER_NAME = EnvironmentConstants.ERC_AMS_AZURE_BLOB_STORAGE_CONTAINER_NAME;
---
+++ PortalDB/Models/ResponseModels/Dashboard/DashboardPTAResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PortalDB.Models.ResponseModels.Dashboard
+{
+    public class DashboardPTAResponseModel
+    {
+        public long TotalPPE { get; set; }
+        public long TotalSE { get; set; }
+        public decimal TotalPPEValue { get; set; }
+        public decimal TotalPPEValuePercentage { get; set; }
+        public decimal TotalSEValue { get; set; }
+        public decimal TotalSEValuePercentage { get; set; }
+    }
+}
