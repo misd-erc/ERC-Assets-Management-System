b697a5245fc48df5e1efe068ec2f43f67f5cc860
unknown <cedrickbarzaga92@gmail.com>
2026-05-06 23:50:03 +0800

    feat(migrations): update DeliveryRecordId to nullable in tblSupplyIARs
    - Added a new migration to alter the DeliveryRecordId column in the tblSupplyIARs table to be nullable.
    - Updated the model snapshot to reflect the change in the DeliveryRecordId property type to long?.
    - Bumped version in .env file to 1.35.83 for staging and production environments.

---
+++ PortalDB/Migrations/20260506150936_NewMigration.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PortalDB.Migrations
+{
+    /// <inheritdoc />
+    public partial class NewMigration : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "DeliveryRecordId",
+                schema: "asset",
+                table: "tblSupplyIARs",
+                type: "bigint",
+                nullable: true,
+                oldClrType: typeof(long),
+                oldType: "bigint");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<long>(
+                name: "DeliveryRecordId",
+                schema: "asset",
+                table: "tblSupplyIARs",
+                type: "bigint",
+                nullable: false,
+                defaultValue: 0L,
+                oldClrType: typeof(long),
+                oldType: "bigint",
+                oldNullable: true);
+        }
+    }
+}
