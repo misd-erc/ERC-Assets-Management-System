1d1531cd7655629c000bbb639df1eeb4f61121a8
unknown <cedrickbarzaga92@gmail.com>
2026-09-01 00:56:12 +0800

    remove unused empty rows in SEPropertyReportGenerator and SESPIExcelGenerator

--- WEB/src/components/assets/reports/SEPropertyReportGenerator.tsx
+++ WEB/src/components/assets/reports/SEPropertyReportGenerator.tsx
@@ -479,15 +479,6 @@ export class SEPropertyReportGenerator {
                 </Text>
               </View>
             ))}
-
-            {/* EMPTY ROWS */}
-            {Array.from({ length: Math.max(0, 20 - finalAssets.length) }).map((_, i) => (
-              <View key={i} style={styles.row}>
-                {Array.from({ length: 8 }).map((_, c) => (
-                  <Text key={c} style={[styles.td, { width: this.colWidth(c) }]} />
-                ))}
-              </View>
-            ))}
           </View>
 
           {/* SIGNATURES */}
