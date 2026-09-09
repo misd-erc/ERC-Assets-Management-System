5b18401f02993009b573224f13b441766f2f55f8
unknown <cedrickbarzaga92@gmail.com>
2026-09-09 22:04:14 +0800

    chore: add VSCode settings file and update UserValidationQueryParams to remove LastName requirement

--- PortalDB/Models/QueryParams/Account/UserValidationQueryParams.cs
+++ PortalDB/Models/QueryParams/Account/UserValidationQueryParams.cs
@@ -7,7 +7,7 @@ namespace PortalDB.Models.QueryParams.Account
     {
         [Required] public long EntraId { get; set; }
         [Required] public string FirstName { get; set; } = string.Empty;
-        [Required] public string LastName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         [Required] public string Email { get; set; } = string.Empty;
         public string EmployeeId { get; set; } = string.Empty;
     }
